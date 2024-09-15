2c2fa9665fc84491a014ca462d191fdd90dbc7e1
Macfe1 <mac1515.pp@gmail.com>
2024-09-15 10:02:32 -0500

    a

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -3,7 +3,7 @@ number = 3.14159
 
 try:
     cut_the_number = round(number, 2)
-except(TypeError, ValueError) as round_err:
+except (TypeError, ValueError) as round_err:
     print(f"Error in round: {round_err}")
     cut_the_number = None
 try:
@@ -12,4 +12,4 @@ try:
     else:
         print("ValueError: Unknown format code 'f' for object of type 'str'")
 except (TypeError, ValueError) as print_err:
-    print (f"Error to print: {print_err}")
+    print(f"Error to print: {print_err}")
