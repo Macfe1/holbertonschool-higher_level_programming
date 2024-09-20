448f6418938d89d5bcba5da66d8be91fd2f2ec49
Macfe1 <mac1515.pp@gmail.com>
2024-09-20 00:05:29 -0500

    Adding the file 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for letras in range(len(str)):
+        to_upper = ord(str[letras])
+        if to_upper > 96 and to_upper < 123:
+            to_upper = to_upper - 32
+        new_str += chr(to_upper)
+    print(f"{new_str}")
