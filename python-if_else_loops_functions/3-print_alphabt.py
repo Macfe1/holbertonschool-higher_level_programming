bad6c8340e53c95f69b856ec242ab5253f28ed8e
Macfe1 <mac1515.pp@gmail.com>
2024-09-19 20:39:56 -0500

    Adding the 3-print_alphabt.py with style

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(letter) for letter in range(97, 123) if letter != 101 and letter != 113)), end="")
-
+print(
+        "{}".format(
+            "".join(
+                chr(letter) for letter in range(97, 123)
+                if letter != 101 and letter != 113)), end="")
