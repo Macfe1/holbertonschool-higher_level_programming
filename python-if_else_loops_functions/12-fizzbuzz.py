c01f1b6094f7bcc9df04f87c1fe9b54c6da62f8b
Macfe1 <mac1515.pp@gmail.com>
2024-09-20 19:25:30 -0500

    Adding the file 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for numero in range(1, 100):
+
+        if numero % 3 == 0 and numero % 5 == 0:
+            print("FizzBuzz", end=" ")
+
+        elif numero % 3 == 0:
+            print("Fizz", end=" ")
+        elif numero % 5 == 0:
+            print("Buzz", end= " ")
+        
+        print(numero, end=" ")
