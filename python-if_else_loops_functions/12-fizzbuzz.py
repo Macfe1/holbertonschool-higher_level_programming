8583625bf16de6b62e3415eb2226b16aa03623ce
Macfe1 <mac1515.pp@gmail.com>
2024-09-20 19:32:48 -0500

    Adding the file 12 with the format required and style with additional number

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for numero in range(1, 100):
+    for numero in range(1, 101):
 
         if numero % 3 == 0 and numero % 5 == 0:
             print("FizzBuzz", end=" ")
