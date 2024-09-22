363c74be65abb82f13499c011ed508026d1dddf7
Macfe1 <mac1515.pp@gmail.com>
2024-09-21 19:34:42 -0500

    Adding 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    number_args = len(sys.argv) - 1
+
+    if number_args == 0:
+        print("0 arguments.")
+    elif number_args == 1:
+        print("1 argument:")
+    elif number_args > 1:
+        print("{} argument:".format(number_args))
+
+    for iterator in range(1, len(sys.argv)):
+        print("{}: {}".format(iterator, sys.argv[iterator]))
