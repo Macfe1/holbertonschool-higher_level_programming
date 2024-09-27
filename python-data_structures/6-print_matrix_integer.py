f2460189d135113a3677aad8f66045d5e2882d4f
Macfe1 <mac1515.pp@gmail.com>
2024-09-26 20:16:07 -0500

    Adding the file 6-print_matrix_integer.py with .format and :d and without the final space and the right format and the style

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
-        for iterator, column in enumerate (row):
+        for iterator, column in enumerate(row):
             if iterator < len(row) - 1:
                 print("{:d}".format(column), end=" ")
             else:
