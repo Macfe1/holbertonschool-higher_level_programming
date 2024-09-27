dfe78034954af0cd6d9f604ea391dfa5860ccf6f
Macfe1 <mac1515.pp@gmail.com>
2024-09-26 20:44:43 -0500

    Adding 7-add_tuple.py, with the settings in the index of tuple_b[1]

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,6 +3,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     element_a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     element_a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     element_b1 = tuple_b[0] if len(tuple_b) > 0 else 0
-    element_b2 = tuple_b[0] if len(tuple_b) > 1 else 0
+    element_b2 = tuple_b[1] if len(tuple_b) > 1 else 0
 
     return element_a1 + element_b1, element_a2 + element_b2
