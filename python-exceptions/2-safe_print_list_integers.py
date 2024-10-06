394398efc4ea911949304fd9dd4f194038e84ee5
Macfe1 <mac1515.pp@gmail.com>
2024-10-05 21:40:31 -0500

    Adding the file 2 with a second variable

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
+    counter_print = 0
 
     while counter < x:
         try:
             print("{:d}".format(my_list[counter]), end="")
-            counter += 1
+            counter_print += 1
         except (ValueError, TypeError):
-            counter += 1
+            pass
         except IndexError:
             break
+        counter += 1
     print()
-    return counter
+    return counter_print
 
 
 if __name__ == "__main__":
