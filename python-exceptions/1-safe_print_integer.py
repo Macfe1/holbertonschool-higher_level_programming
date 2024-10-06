c0563495244023f684080b7077fb35a8f3a76179
Macfe1 <mac1515.pp@gmail.com>
2024-10-05 20:39:01 -0500

    Addign file 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    if isinstance(value, int):
+        try:
+            print("{:d}".format(value))
+            return True
+        except TypeError:
+            print("Error")
+            return False
+
+
+if __name__ == "__main__":
+    safe_print_integer(89)
