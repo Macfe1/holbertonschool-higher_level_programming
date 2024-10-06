b3ab6b00dcdca3f3e2aafa8813a6dc3feb8623a4
Macfe1 <mac1515.pp@gmail.com>
2024-10-05 22:50:47 -0500

    Addign teh file 1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -17,4 +17,13 @@ def safe_print_list_integers(my_list=[], x=0):
 
 
 if __name__ == "__main__":
-    safe_print_list_integers([1, 2, 3, "Hola"], 4)
+    my_list = [1, 2, 3, 4, 5]
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
