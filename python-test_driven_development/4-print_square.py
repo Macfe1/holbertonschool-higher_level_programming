6111a0504cd0cc274f33795eebf3badd827fe279
Macfe1 <mac1515.pp@gmail.com>
2024-10-06 21:20:07 -0500

    Adding the file 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    
+    for row in range(size):
+        print("#" * size)
+
+if __name__ == "__main__":
+    print_square(5)
