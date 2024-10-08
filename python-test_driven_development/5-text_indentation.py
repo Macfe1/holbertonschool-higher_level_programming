5364a82386822c7b107e8377c966a457b66bd28f
Macfe1 <mac1515.pp@gmail.com>
2024-10-08 03:27:35 -0500

    Adjusting the file number 5 in the print

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,36 @@
 #!/usr/bin/python3
 """
-This module contains a function that formats a 
-given text by adding two new lines after each occurrence 
+This module contains a function that formats a
+given text by adding two new lines after each occurrence
 of the following punctuation marks: '.', '?', and ':'.
 """
 
 
 def text_indentation(text):
     """
-    Formats the given text by ensuring there are 
-    two new lines after each of the specified 
+    Formats the given text by ensuring there are
+    two new lines after each of the specified
     punctuation marks. Removes any trailing spaces from the output lines
 
     Parameters:
     text (str): The text to be formatted.
-    
+
     Print:
     The formatted text
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     for symbol in '.?:':
         text = text.replace(symbol, symbol + "\n")
 
-    lines_divided = [line.strip() for line in text.splitlines() if line.strip()]
-    
+    lines_divided = [
+            line.strip() for line in text.splitlines() if line.strip()
+    ]
+
     for line in lines_divided:
-        print(line)
+        print(line, end="")
+
+
+if __name__ == "__main__":
+    text_indentation("Holberton School")
