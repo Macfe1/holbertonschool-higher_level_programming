3367897568cf9b311b0247dd18912ee5309309b3
Macfe1 <mac1515.pp@gmail.com>
2024-10-08 00:18:15 -0500

    Adding the file number 4 and the file number 4 in tests folder

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,14 +1,39 @@
 #!/usr/bin/python3
+"""
+In this module we'll print a square with the character #
+a size number of times
+"""
+
+
 def print_square(size):
+    """
+    With this fucntion we will print a square with character # printed
+    size times
+
+    Args:
+    size(int): The size length of the square
+
+    Print:
+    A square with the size length
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    
+    if size == 0:
+        return
     for row in range(size):
         print("#" * size)
 
+
 if __name__ == "__main__":
-    print_square(5)
+    print_square(4)
+    print("")
+    print_square(10)
+    print("")
+    print_square(0)
+    print("")
+    print_square(1)
+    print("")
