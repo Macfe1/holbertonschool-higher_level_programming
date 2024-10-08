2624d24bd95ec0cbcb54595f7593f94e36d3d52d
Macfe1 <mac1515.pp@gmail.com>
2024-10-08 04:15:24 -0500

    Adding 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""
+Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-     def test_usual_cases(self):
+    def test_usual_cases(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
         self.assertEqual(max_integer([-1, -2, -3]), -1)
@@ -13,11 +14,11 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_empty_list(self):
         self.assertIsNone(max_integer([]))
-
+    
     def test_single_number(self):
         self.assertEqual(max_integer([42]), 42)
         self.assertEqual(max_integer([-10]), -10)
-
+    
     def test_identical_numbers(self):
         self.assertEqual(max_integer([5, 5, 5, 5]), 5)
 
