f33e14a5fc535c904c5460f9cca82eaadab66f8a
Macfe1 <mac1515.pp@gmail.com>
2024-10-08 01:29:12 -0500

    Adding the file number 2 and the file in the folder tests named 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+In this module will take a matrix and
+divides every number in there with the number div argument
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    This function snip every number of the matrix and
+    it is divided by the argument div tis division returns
+    a new matrix
+
+    Args:
+    matrix(int, float): list of list of integers or floats
+    div(int, float): number
+
+    Return:
+    a new matrix wiht the result of the division
+    """
+
+    if not isinstance(matrix, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for number in row:
+            if not isinstance(number, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    row_length = len(matrix[0])
+    for rows in matrix[1:]:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(number / div, 2) for number in row]
+        new_matrix.append(new_row)
+    
+    return new_matrix
