046772afe97878528c350a3716a29e7161fb1f74
Macfe1 <mac1515.pp@gmail.com>
2024-10-09 21:34:44 -0500

    With the docuemntation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square.
+
+The Square class is currently empty 
+"""
 class Square:
+    """
+    An empty class that defines a square.
+    """
     pass
