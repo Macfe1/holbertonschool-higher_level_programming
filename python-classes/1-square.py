b10decf3007275fa89990503933f1400df7844d8
Macfe1 <mac1515.pp@gmail.com>
2024-10-09 22:38:07 -0500

    Adding the file number 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-The module have a class called in the 0-square file
-and defines the Square class with the size
+This module defines a class Square that represents a square.
 """
-from 0-square import Square
 
 
 class Square:
     """
-     A class that defines a square by its size.
+    A class that defines a square by its size.
     """
-    def __init__(self, size)
-
-    """
-    Initializes a new Square instance with the given size.
-    Arguments:size: The size of the square.
-    """
-    self.__size = size
+    def __init__(self, size):
+        """
+        Initializes a new Square instance with the given size.
+        Arguments:size: The size of the square.
+        """
+        self.__size = size
