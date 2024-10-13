ebfcb5b3cf3c1e0e54114d39cee76db56bfa26a8
Macfe1 <mac1515.pp@gmail.com>
2024-10-12 21:58:58 -0500

    Adding teh file 0 and the README

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle
+The Rectangle class is empty
+"""
+class Rectangle:
+    """
+    An empty class that defines a Rectangle
+    """
+    pass
