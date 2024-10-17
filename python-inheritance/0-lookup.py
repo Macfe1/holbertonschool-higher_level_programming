daeee7fef58cffbc7ec9212888aace90a9bdeca7
Macfe1 <mac1515.pp@gmail.com>
2024-10-16 22:11:33 -0500

    Adding teh file number 0 with style

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-This module have a method that returns 
+This module have a method that returns
 the list of available attributes and methods of an object:
 """
+
+
 def lookup(obj):
     """
-    This function returns the list of available attributes 
+    This function returns the list of available attributes
     and methods of an object:
     """
     return dir(obj)
