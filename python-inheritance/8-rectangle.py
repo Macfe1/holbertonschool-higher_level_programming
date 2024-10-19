ad4483e9ec6221059ef42ec86dd1d0096ef6c4a0
Macfe1 <mac1515.pp@gmail.com>
2024-10-18 23:13:09 -0500

    Adding the file number 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module have a class with
+a method that raise an exception
+"""
+
+
+class BaseGeometry:
+    """
+    This is a class with a method that
+    raise an exception
+    """
+    def integer_validator(self, name, value):
+        """
+        This is a method that checks if the value
+        is an integer that is positive and no cero
+
+        Args:
+
+            name(str): a name
+            value(int): a number
+
+        TypeError:
+            If value is not a integer
+        ValueError:
+            If value is negative or cero
+        """
+        if type(value) is bool:
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+    def area(self):
+        """
+        method that raise and exception with a message
+        """
+        raise Exception("area() is not implemented")
+    
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
