aba2efa1aac7da7e111b2f868a08e1d6fb5d63f6
Macfe1 <mac1515.pp@gmail.com>
2024-10-20 15:37:54 -0500

    Assing teh file task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -3,27 +3,54 @@ from abc import ABC, abstractmethod
 
 
 class Shape(ABC):
+    """
+    Abstract base class for shapes.
+    Defines the blueprint for shape objects by enforcing
+    the implementation of area and perimeter methods.
+    """
     @abstractmethod
     def area(self):
+        """
+        Calculate and return the
+        area of the shape.
+        """
         pass
 
     @abstractmethod
     def perimeter(self):
+        """
+        Calculate and return the
+        perimeter of the shape.
+        """
         pass
 
 
 class Circle(Shape):
+    """
+    A class representing a circle.
+    """
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
+        """
+        Calculate and return
+        the area of the circle.
+        """
         return 3.1416 * (self.radius * self.radius)
 
     def perimeter(self):
+        """
+        Calculate and return
+        the perimeter of the circle.
+        """
         return 2 * self.radius * 3.1416
 
 
 class Rectangle(Shape):
+    """
+    A class representing a Rectangle
+    """
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -36,5 +63,11 @@ class Rectangle(Shape):
 
 
 def shape_info(object_o):
+    """
+    Prints the area and perimeter of the shape passed in.
+
+    Args:
+        object_o (Shape): The shape object (Circle or Rectangle) to calculate.
+    """
     print(f"Area: {object_o.area()}")
     print(f"Perimeter: {object_o.perimeter()}")
