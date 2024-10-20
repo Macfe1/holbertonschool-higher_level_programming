caad4e6977556583537b7d6f6898f968e73e4ce3
Macfe1 <mac1515.pp@gmail.com>
2024-10-19 20:37:00 -0500

    Adding teh file 9 with import

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,38 +5,7 @@ a method that raise an exception
 """
 
 
-class BaseGeometry:
-    """
-    This is a class with a method that
-    raise an exception
-    """
-    def integer_validator(self, name, value):
-        """
-        This is a method that checks if the value
-        is an integer that is positive and not cero
-
-        Args:
-
-            name(str): a name
-            value(int): a number
-
-        TypeError:
-            If value is not a integer
-        ValueError:
-            If value is negative or cero
-        """
-        if type(value) is bool:
-            raise TypeError(f"{name} must be an integer")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-    def area(self):
-        """
-        method that raise and exception with a message
-        """
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
