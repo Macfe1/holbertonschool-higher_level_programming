2f65b539945183020da3c91952a91e50f39379da
Macfe1 <mac1515.pp@gmail.com>
2024-10-19 20:33:46 -0500

    Adding the file mnumbe r8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,38 +5,7 @@ a method that raise an exception
 """
 
 
-class BaseGeometry:
-    """
-    This is a class with a method that
-    raise an exception
-    """
-    def integer_validator(self, name, value):
-        """
-        This is a method that checks if the value
-        is an integer that is positive and no cero
-
-        Args:
-
-            name(str): a name
-            value(int): a number
-
-        TypeError:
-            If value is not a integer
-        ValueError:
-            If value is negative or cero
-        """
-        if type(value) is bool:
-            raise TypeError(f"{name} must be an integer")
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-    def area(self):
-        """
-        method that raise and exception with a message
-        """
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -63,7 +32,6 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
 
