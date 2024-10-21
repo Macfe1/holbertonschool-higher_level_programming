6863164d44ad6ec60800c6988e32f1a6954da54e
Macfe1 <mac1515.pp@gmail.com>
2024-10-20 20:17:46 -0500

    git add task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+A module that defines a class
+for creating an iterator.
+"""
+
+
+class CountedIterator:
+    """
+    An iterator that counts how many items have been iterated.
+
+    Attributes:
+        iterator(int): An iterator object created from an iterable.
+        counter(int): A counter that tracks the number of items iterated.
+
+    Methods:
+        get_count(): Returns the number of items that have been iterated.
+        __next__(): Retrieves the next item
+        from the iterator and increments the counter.
+    """
+    def __init__(self, list_iter):
+        """
+        Initializes the CountedIterator with an iterable.
+
+        Args:
+            iterable (iterable): A data structure
+            that can be iterated (list, tuple, etc.)
+        """
+        self.iterator = iter(list_iter)
+        self.counter = 0
+
+    def getcount(self):
+        """
+        Returns the number of items that have been iterated.
+
+        Returns:
+            int: The number of items iterated so far.
+        """
+        return self.counter
+
+    def __next__(self):
+        """
+        Returns the next item from the iterator and increments the counter.
+
+        Returns:
+            object: The next item from the iterable.
+
+        Raises:
+            StopIteration: If there are no more items to iterate.
+        """
+        next_item = next(self.iterator)
+        self.counter += 1
+
+        return next_item
+
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4]
+    counted_iter = CountedIterator(data)
+
+    try:
+        while True:
+            item = next(counted_iter)
+            print(f"Got {item}, total {counted_iter.getcount()}\
+                    items iterated.")
+    except StopIteration:
+        print("No more items.")
