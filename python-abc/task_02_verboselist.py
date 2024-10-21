fca2157b755ccfaaeddb7c2b4ba4b91650528ff2
Macfe1 <mac1515.pp@gmail.com>
2024-10-20 19:24:20 -0500

    Adding the file task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 class VerboseList(list):
+
     def append(self, item):
         super().append(item)
-        print(f"Added [{item}] to the list")
+        print(f"Added [{item}] to the list.")
 
     def extend(self, items):
         super().extend(items)
         print(f"Extended the list with [{len(items)}] items.")
 
     def remove(self, item):
-        if item in self:
-            print(f"Removed [{item}] from the list")
-            super().remove(item)
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
 
     def pop(self, index=-1):
         print(f"Popped [{self[index]}] from the list.")
