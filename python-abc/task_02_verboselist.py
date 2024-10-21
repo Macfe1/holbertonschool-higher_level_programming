71c7040456ea422282e90eeb6ba1d485dfb3a5cc
Macfe1 <mac1515.pp@gmail.com>
2024-10-20 19:05:15 -0500

    Adding the file task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,23 +9,19 @@ class VerboseList(list):
         print(f"Extended the list with [{len(items)}] items.")
 
     def remove(self, item):
-        print(f"Removed [{item}] from the list")
-        super().remove(item)
+        if item in self:
+            print(f"Removed [{item}] from the list")
+            super().remove(item)
 
     def pop(self, index=-1):
-        print(f"Popped [{index}] from the list.")
+        print(f"Popped [{self[index]}] from the list.")
         super().pop(index)
 
 
 if __name__ == "__main__":
-    type_list = VerboseList([1, 2, 3])
-    print(type_list)
-    type_list.append(4)
-    type_list.extend([5, 6, 7])
-    print(type_list)
-    type_list.remove(7)
-    print(type_list)
-    type_list.pop()
-    print(type_list)
-    type_list.pop(2)
-    print(type_list)
+    vl = VerboseList([1, 2, 3])
+    vl.append(4)
+    vl.extend([5, 6])
+    vl.remove(2)
+    vl.pop()
+    vl.pop(0)
