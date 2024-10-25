9258a04b44d9cf86ed7ea0003620e257c0638d55
Macfe1 <mac1515.pp@gmail.com>
2024-10-24 21:09:43 -0500

    Adding teh file number 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a function that creates
+an Object from a “JSON file”
+"""
+import json
+
+
+def load_from_json_file(filename):    
+    """
+    Creates an Object from a “JSON file”
+
+    Args:
+        filename: The name of the JSON file
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
