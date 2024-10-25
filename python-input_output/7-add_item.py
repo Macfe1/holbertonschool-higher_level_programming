07630170bd7d0a0e71ad382b289c66461ac20940
Macfe1 <mac1515.pp@gmail.com>
2024-10-24 23:02:06 -0500

    Addign teh file number 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+The module have a function that adds
+all arguments to a Python list,
+and then save them to a JSON file:
+"""
+
+
+import sys
+import json
+
+save_to_json_fl = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def from_pylist_to_jsonlist():
+
+    try:
+        items = load_from_json_file("add_item.json")
+
+    except FileNotFoundError:
+        items = []
+        print(items)
+
+    items.append(sys.argv[1:])
+    save_to_json_fl(items, "add_item.json")
+
+
+if __name__ == "__main__":
+    from_pylist_to_jsonlist()
