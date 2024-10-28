c5448e4d5777b3a94452d1f7174b27aa1424bbeb
Macfe1 <mac1515.pp@gmail.com>
2024-10-27 21:17:32 -0500

    Addign the file task_03_xml.py

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module provides functionality for serializing
+and deserializing Python dictionaries
+to and from XML format.
+"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """Serialize a Python dictionary to an XML file."""
+
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        item = ET.SubElement(root, "key")
+        item.text = str(value)
+
+    tree = ET.ElementTree(root)
+    with open(filename, "wb") as xml_File:
+        tree.write(xml_file)
+
+
+def deserialize_from_xml(filename):
+    """Deserialize an XML file back into a Python dictionary."""
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    dictionary = {}
+
+    for item in root:
+        dictionary[item.tag] = item.text
+
+    return dictionary
