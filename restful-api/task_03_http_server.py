63471101b3f2217995339c990e9be448e849ceec
Macfe1 <mac1515.pp@gmail.com>
2024-11-03 11:56:36 -0500

    Adding teh file task_03_http_server.py

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,32 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        if self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(
+                    b"404 Not Found: The requested resource was not found.")
+
+
+server_address = ('localhost', 8000)
+server = HTTPServer(server_address, HTTPHandler)
+print("Server Running on http://localhost:8000")
+server.serve_forever()
