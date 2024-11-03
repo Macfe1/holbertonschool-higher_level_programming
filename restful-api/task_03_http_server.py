b64b5aeee8e299d1a226bef104772b741ce28441
Macfe1 <mac1515.pp@gmail.com>
2024-11-03 12:22:10 -0500

    Adding teh file task_03_http_server.py fixing the /info

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,7 +5,14 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class HTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
 
-        if self.path == "/data":
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Simple API")
+
+
+        elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
@@ -17,6 +24,13 @@ class HTTPHandler(BaseHTTPRequestHandler):
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
+        
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            data = {"version": "1.0", "description": "A simple API built with http.server"}  
+            self.wfile.write(json.dumps(data).encode("utf-8"))
 
         else:
             self.send_response(404)
