465235ec0d0ede3547f0bac53f6ddaf00fd0136b
Macfe1 <mac1515.pp@gmail.com>
2024-11-05 00:38:22 -0500

    Adding the file 2 managing errora

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
         cursor = db.cursor()
 
-        query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
+        query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
         cursor.execute(query, (state_name,))
 
         states = cursor.fetchall()
