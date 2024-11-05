353db88f9dc2753c0891b8bcbda13a9a893f4d9b
Macfe1 <mac1515.pp@gmail.com>
2024-11-04 22:53:45 -0500

    Adding the file 1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database using credentials.
+It retrieves and displays all states from the 'states' table
+where the name starts with 'N',
+ordered by their 'id' in ascending order.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Retrieves MySQL credentials and the database
+    name from command-line arguments.
+    Connects to the specified MySQL database,
+    executes a query to select all states
+    whose names start with 'N', ordered by 'id'
+    in ascending order, and prints each result.
+    """
+
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_user,
+            passwd=mysql_password,
+            db=database_name,
+            )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'ORDER BY id ASC")
+
+    states_n = cursor.fetchall()
+
+    for iter_state in states_n:
+        print(iter_state)
+
+    cursor.close()
+    db.close()
