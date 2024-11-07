c988aa22b46f4cd9ab0cc45863f3c6d1b10ac528
Macfe1 <mac1515.pp@gmail.com>
2024-11-06 20:58:16 -0500

    with the database created

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,7 @@
 -- script that creates a table called first_table in the current database in the MySQL server
+CREATE DATABASE hbtn_0c_0;
+
+-- Select the database
 USE hbtn_0c_0;
 
 -- Create a table in the database 
