6f18b49ff3e76ad93c0983bc0d2fd65afcfa89ee
Macfe1 <mac1515.pp@gmail.com>
2024-11-06 21:16:21 -0500

    Adding teh file 4-first_table.sql

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,10 +1,4 @@
 -- script that creates a table called first_table in the current database in the MySQL server
-CREATE DATABASE hbtn_0c_0;
-
--- Select the database
-USE hbtn_0c_0;
-
--- Create a table in the database 
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
