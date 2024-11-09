d67634fd08c88442dfc834ffa4a277f16991eba4
Macfe1 <mac1515.pp@gmail.com>
2024-11-08 19:00:15 -0500

    Adding the file 1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- The script creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
