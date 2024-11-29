4598db677a56d1b24479441b15c8f53aafc8cc61
Macfe1 <mac1515.pp@gmail.com>
2024-11-29 00:59:01 -0500

    Adjusting task_02_logic.py

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -5,9 +5,21 @@ app = Flask(__name__)
 
 @app.route('/items')
 def items():
-    with open('items.json', 'r') as read_file:
-        json_to_python_file = json.load(read_file)
-    return render_template('items.html', items=json_to_python_file['items'])
+    try:
+        if os.path.exists('items.json') and os.path.getsize('items.json') > 0:
+            with open('items.json', 'r') as read_file:
+                json_to_python_file = json.load(read_file)
+            
+            items_list = json_to_python_file.get('items', [])
+
+        else:
+            items_list = []
+        
+        return render_template('items.html', items=items_list)
+
+    except Exception as error:
+        print(f"Error: {error}")
+        return render_template('items.html', items=[])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
