4d1c1b2d5eff369f789c8cf4d6d245ccb3e677eb
Macfe1 <mac1515.pp@gmail.com>
2024-11-30 14:18:57 -0500

    Adding teh files product_display.html, task_03, products.csv and json

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import json, csv, os
+
+app = Flask(__name__)
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    id = request.args.get('id')
+
+    if source == 'json':
+        try:
+            with open('products.json', 'r') as json_file:
+                python_file = json.load(json_file)
+        
+        except FileNotFoundError:
+            return render_template('product_display.html', error_message="JSON file not found")
+
+    elif source == 'csv':
+        try:
+            with open('products.csv', 'r') as csv_file:
+                reader = csv.reader(csv_file)
+                python_file = [row for row in reader]
+
+        except FileNotFoundError:
+            return render_template('product_display.html', error_message="CSV file not found")
+
+    else:
+        return render_template('product_display.html', error_message="Wrong source")
+
+    if id:
+        python_file = [product for product in python_file if str(product['id']) == id]
+        if not python_file:
+            return render_template('product_display.html', error_message="Product not found")
+
+    return render_template('product_display.html', products=python_file)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
